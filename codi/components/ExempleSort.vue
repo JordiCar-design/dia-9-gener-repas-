83132db010c6b28592956dede7ca74257c3c9d66
JordiCar-design <jordicll@gmail.com>
2023-01-09 15:53:01 +0100

    1er commit

---
+++ codi/components/ExempleSort.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>Exemple sort</h3>
+        <pre>
+            {{ edats }}
+        </pre>
+    </div>
+</template>
+
+<script>
+export default{
+    mounted(){
+        const nums = [10, 5, 8, 1, 7]
+
+        nums.sort(
+
+            (a, b) => {
+                return a - b
+            }
+        )
+
+        console.log(nums) // [1, 10, 5, 7, 8]
+        this.edats = nums
+    },
+    data(){
+        return {
+            // Estat del component
+            edats: null
+        }
+    }
+}
+</script>
---
+++ codi/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container>
+    <v-col>
+      <h1>Repàs</h1>
+      <exemple-sort></exemple-sort>
+    </v-col>
+  </v-container>
+</template>
